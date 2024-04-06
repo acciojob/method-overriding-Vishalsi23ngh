3ce13179c69ab774c05c91f1f8182c6d51399c69
Vishal Singh <vishalsingh23101999@gmail.com>
2024-04-06 22:05:41 +0530

    This is first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+        String resultA = objB.meth();
+        System.out.println("Result from class A method: " + resultA);
+
+        String resultB = objB.meth();
+        System.out.println("Result from class B method: " + resultB);
+    }
 }
